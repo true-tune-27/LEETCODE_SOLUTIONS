a10a4f914963ca04ad0b9325bdfa080ec75f865b
MANJU MUKESH DANNINA <24P31A0585@acet.ac.in>
2026-07-12 20:43:41 +0530

    Time: 64 ms (58.62%), Space: 173.3 MB (47.78%) - LeetHub

---
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+           vector<vector<int>> grid(m, vector<int>(n, 0));
+        for (auto &w : walls) grid[w[0]][w[1]] = 2;
+        for (auto &g : guards) grid[g[0]][g[1]] = 1;
+
+        const int dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+
+        for (auto &g : guards) {
+            int r = g[0], c = g[1];
+            for (int d = 0; d < 4; ++d) {
+                int nr = r + dirs[d][0], nc = c + dirs[d][1];
+                while (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                    if (grid[nr][nc] == 2 || grid[nr][nc] == 1) break;
+                    if (grid[nr][nc] == 0) grid[nr][nc] = 3;
+                    nr += dirs[d][0];
+                    nc += dirs[d][1];
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (grid[i][j] == 0) ++ans;
+        return ans;
+    }
+};
