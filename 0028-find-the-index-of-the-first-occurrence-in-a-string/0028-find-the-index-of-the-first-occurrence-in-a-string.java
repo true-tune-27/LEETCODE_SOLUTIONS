bb3793c38aeb7ac88c07066e441378c811e2a5e2
MANJU MUKESH DANNINA <24P31A0585@acet.ac.in>
2026-07-12 20:46:00 +0530

    Time: 0 ms (100%), Space: 41.5 MB (99.97%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        haystack.contains(needle);
+        int index = haystack.indexOf(needle);
+        return index;
+    }
+}
