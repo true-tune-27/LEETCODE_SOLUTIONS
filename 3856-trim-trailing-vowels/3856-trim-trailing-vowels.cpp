96103f3fa05dfe1c9617f5251e66f54372132e86
MANJU MUKESH DANNINA <24P31A0585@acet.ac.in>
2026-07-12 19:57:15 +0530

    Time: 0 ms (100%), Space: 8.6 MB (74.68%) - LeetHub

---
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string trimTrailingVowels(string s) {
+        int n = s.size();
+       for(int i = n-1 ; i >= 0 ; i--)
+    {
+    if(s[i] =='a'||s[i] =='e'||s[i] =='i'||s[i] =='o'||s[i] =='u')
+    {
+        s.erase(i,1);
+    }
+        else
+    {
+        break;
+    }
+        
+    }
+        return s;
+    }
+};
