81132c479096f0bfa092be2e0b03906595086005
MANJU MUKESH DANNINA <24P31A0585@acet.ac.in>
2026-07-12 19:56:15 +0530

    Time: 1 ms (93.87%), Space: 42.6 MB (63.38%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int a = Math.abs(z-x);
+        int b = Math.abs(z-y);
+        if(a < b) return 1;
+        else if(a > b) return 2;
+        else return 0;
+    }
+}
