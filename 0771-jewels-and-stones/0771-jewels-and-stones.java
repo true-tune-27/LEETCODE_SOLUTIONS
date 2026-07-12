39af450bf2084ff98401fdaad023057486636929
MANJU MUKESH DANNINA <24P31A0585@acet.ac.in>
2026-07-12 19:56:33 +0530

    Time: 0 ms (100%), Space: 43 MB (72.8%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int cnt = 0;
+        char[] arr1 = jewels.toCharArray();
+        char[] arr2 = stones.toCharArray();
+        for(int i = 0 ; i < arr1.length ; i++)
+        {
+            for(int j = 0 ; j < arr2.length ; j++)
+            {
+                if(arr1[i] == arr2[j])
+                {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
