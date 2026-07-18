3f723422d9281daa4972c6d3955148da17eaa373
MANJU MUKESH DANNINA <24P31A0585@acet.ac.in>
2026-07-18 08:09:09 +0530

    Time: 6 ms (18.9%), Space: 45.6 MB (14.62%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -1,27 +1,18 @@
 class Solution {
-    static int findGCD(int a, int b) {
-        if (a == 0)
-            return b;
-        return findGCD(b % a, a);
+    public int gcd(int a , int b)
+    {
+        if(b==0) return a;
+        else return gcd(b , a % b);
     }
     public int findGCD(int[] nums) {
         int n = nums.length;
-        int min_ele = nums[0];
-        int max_ele = nums[0];
+        Arrays.sort(nums);
+        int res =0 ;
         for(int i = 0 ; i < n ; i++)
         {
-            if(min_ele > nums[i])
-            {
-                min_ele = nums[i];
-            }
-              if(max_ele < nums[i])
-            {
-                max_ele = nums[i];
-            }
+            res = gcd(nums[0] , nums[n-1]);        
         }
-        int l = findGCD(min_ele, max_ele);
-        return l;
-
-        
+        return res;
+    
     }
 }
