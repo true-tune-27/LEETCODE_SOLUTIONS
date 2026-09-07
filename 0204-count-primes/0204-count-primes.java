d4fb37193ddca28b92a5cf09f4484c0f28f88c78
MANJU MUKESH DANNINA <24P31A0585@acet.ac.in>
2026-09-07 21:14:13 +0530

    Time: 772 ms (12.96%), Space: 79.7 MB (27.24%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,24 +1,47 @@
+class Solution {
 
-  class Solution {
     public int countPrimes(int n) {
-        boolean[] composite = new boolean[n];
+
+        if (n <= 1) {
+            return 0;
+        }
 
         int count = 0;
 
+        boolean[] isprime = new boolean[n];
+
+        Arrays.fill(isprime, true);
+
+        //traverse untill sqrt(n)
         for (int i = 2; i * i < n; i++) {
-            if (!composite[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    composite[j] = true;
+
+            if (isprime[i] == true) {
+
+                // i's multiples can't be a prime number
+                for (int j = i * i; j < n; j = j + i) {
+
+                    isprime[j] = false;
+
                 }
+
             }
+
         }
 
+        //counting prime
         for (int i = 2; i < n; i++) {
-            if (!composite[i]) {
+
+            if (isprime[i] == true) {
+
                 count++;
+
             }
+
         }
 
         return count;
+
     }
+
 }
+
