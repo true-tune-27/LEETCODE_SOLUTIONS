feb002dc8df2042c18500164ed62c1eb48bdc7ea
MANJU MUKESH DANNINA <24P31A0585@acet.ac.in>
2026-09-12 14:16:13 +0530

    Time: 2 ms (91.72%), Space: 47.7 MB (75.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-          int j  =0;    
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-         
-            if(nums[i] != 0)
-            {
-                nums[j] = nums[i];
-                j++;
+        int j = 0; // Pointer to place the next non-zero element
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;   // Move j to the next index for placing non-zero
             }
         }
-       for(int i = j ; i < nums.length ; i++)
-        {
-            nums[i] = 0;
-           // j++;
-        }
-        System.out.println(nums);
-
     }
 }
