f05b1fd972668c57181ba850c4dab92af3978a64
MANJU MUKESH DANNINA <24P31A0585@acet.ac.in>
2026-09-15 13:34:13 +0530

    Time: 1 ms (99.61%), Space: 45.4 MB (75.77%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer>res = new ArrayList<>();
+        int n = mountain.length;
+        for(int i = 1 ; i < n-1 ; i++)
+        {
+            
+                if(mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1])
+                {
+                    res.add(i);
+                }
+
+        }
+        return res;
+    }
+}
